8fbe99ac9a24702701def0d00a5e181169d6f202
irene5gh <q11476105@takming.edu.tw>
2026-01-31 12:37:18 +0800

    Add files via upload

---
+++ TEST11.cbl
@@ -0,0 +1,16 @@
+       program-id. TEST11.
+
+       environment division.
+       configuration section.
+
+       data division.
+       working-storage section.
+       linkage section.
+
+       procedure division.
+           
+           goback.
+
+       end program TEST11.
+
+       
